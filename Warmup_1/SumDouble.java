6f4ab13d94f549a165fe83e32de351eee116c851
Mohammed Al Maskari <maskari2005@gmail.com>
2025-07-24 15:54:20 +0400

    Add solution for warmup1-sumDouble

---
+++ Warmup_1/SumDouble.java
@@ -0,0 +1,12 @@
+package Warmup_1;
+
+public class SumDouble {
+    public int sumDouble(int a, int b) {
+        if(a==b){
+            return 2*(a+b);
+        }
+        else{
+            return a+b;
+        }
+    }
+}
