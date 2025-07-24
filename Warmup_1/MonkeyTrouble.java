199acae559cb2ad95c955787b9bdf722395c5631
Mohammed Al Maskari <maskari2005@gmail.com>
2025-07-24 15:52:50 +0400

    Add solution for warmup1-monkeyTrouble

---
+++ Warmup_1/MonkeyTrouble.java
@@ -0,0 +1,8 @@
+package Warmup_1;
+
+public class MonkeyTrouble {
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        return (aSmile && bSmile) ||  !(aSmile || bSmile);
+    }
+
+}
