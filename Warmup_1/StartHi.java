c2d70a486a38e24b6f08c71881962ee9758a81fa
Mohammed Al Maskari <maskari2005@gmail.com>
2025-07-28 19:05:13 +0400

    Add solution for warmup1-startHi

---
+++ Warmup_1/StartHi.java
@@ -0,0 +1,10 @@
+package Warmup_1;
+
+public class StartHi {
+    public boolean startHi(String str) {
+        if (str.length()<2){
+            return false;
+        }
+        return str.substring(0,2).equals("hi");
+    }
+}
