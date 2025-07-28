986148ef023c61d4ee0d5ed8f4b66e244ce7e873
Mohammed Al Maskari <maskari2005@gmail.com>
2025-07-28 18:14:45 +0400

    Add solution for warmup1-front3

---
+++ Warmup_1/Front3.java
@@ -0,0 +1,12 @@
+package Warmup_1;
+
+public class Front3 {
+    public String front3(String str) {
+        if (str.length() <= 3){
+            return str + str + str;
+        }
+        String front = str.substring(0,3);
+        return front + front + front;
+    }
+
+}
