21d5a53b00e6b68b7be27e69f660a14aa7f23293
Mohammed Al Maskari <maskari2005@gmail.com>
2025-07-28 20:57:06 +0400

    Add solution for warmup1-loneTeen

---
+++ Warmup_1/LoneTeen.java
@@ -0,0 +1,7 @@
+package Warmup_1;
+
+public class LoneTeen {
+    public boolean loneTeen(int a, int b) {
+        return (a >= 13 && a <= 19 && (b < 13 || b> 19)) || (b >= 13 && b <= 19 && (a < 13 || a> 19));
+    }
+}
